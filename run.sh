071ce50e59d2e448b20896ce2b1bf18d0b4b5bf5
Atnafu Lambebo Tonja <atnafuatx@gmail.com>
2023-06-21 01:11:28 -0600

    init

--- run.sh
+++ run.sh
@@ -1,22 +1,24 @@
 export MAX_LENGTH=164
-export BERT_MODEL=Atnafu/amh-base-LAFT
-export LABEL=/home/atnafuatx/masakhane-pos/data/amh
-export OUTPUT_DIR=amh_pos
+export LABEL=home/atnafuatx/masakhane-pos/data/amh/
+export OUTPUT_DIR=amh_pos_${BERT_MODEL}
 export BATCH_SIZE=32
-export NUM_EPOCHS=10
+export NUM_EPOCHS=5
 export SAVE_STEPS=10000
 export SEED=1
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train_pos.py --data_dir data/amh/ \
---model_type xlmroberta \
---model_name_or_path $BERT_MODEL \
---labels  $LABEL \
---output_dir $OUTPUT_DIR \
---max_seq_length  $MAX_LENGTH \
---num_train_epochs $NUM_EPOCHS \
---per_gpu_train_batch_size $BATCH_SIZE \
---save_steps $SAVE_STEPS \
---seed $SEED \
---do_train \
---do_eval \
---do_predict
+for model in Atnafu/amh-base-LAFT Atnafu/amh_large-LAFT Atnafu/amh_small-LAFT
+do
+	export BERT_MODEL=${model}
+        CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train_pos.py --data_dir data/amh/ \
+        --model_type xlmroberta \
+        --model_name_or_path $BERT_MODEL \
+        --labels  $LABEL \
+        --output_dir $OUTPUT_DIR \
+        --max_seq_length  $MAX_LENGTH \
+        --num_train_epochs $NUM_EPOCHS \
+        --per_gpu_train_batch_size $BATCH_SIZE \
+        --save_steps $SAVE_STEPS \
+        --seed $SEED \
+        --do_train \
+        --do_eval \
+        --do_predict
+done
