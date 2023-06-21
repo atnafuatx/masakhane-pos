1b495af602fbbd7741c9723f1f348a470e74a728
Atnafu Lambebo Tonja <atnafuatx@gmail.com>
2023-06-21 00:29:14 -0600

    init

---
+++ run.sh
@@ -0,0 +1,20 @@
+export MAX_LENGTH=164
+export BERT_MODEL=Atnafu/amh-base-LAFT
+export OUTPUT_DIR=amh_pos
+export BATCH_SIZE=32
+export NUM_EPOCHS=10
+export SAVE_STEPS=10000
+export SEED=1
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train_pos.py --data_dir data/amh/ \
+--model_type xlmroberta \
+--model_name_or_path $BERT_MODEL \
+--output_dir $OUTPUT_DIR \
+--max_seq_length  $MAX_LENGTH \
+--num_train_epochs $NUM_EPOCHS \
+--per_gpu_train_batch_size $BATCH_SIZE \
+--save_steps $SAVE_STEPS \
+--seed $SEED \
+--do_train \
+--do_eval \
+--do_predict
